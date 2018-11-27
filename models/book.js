b387c88bd2e0286eba2b08f6fbfd91c74dbbe827
sahenke13 <shenke13@gmail.com>
2018-11-26 19:07:40 -0600

    commited to heroku

--- models/book.js
+++ models/book.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+
 const bookSchema = new Schema({
     title: { type: String, require: true},
     authors: {type: String, require: true},
